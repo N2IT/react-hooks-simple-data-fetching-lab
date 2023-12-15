99654b63b6f9a4b6ae27ddb606896e3899207bbe
Tony Eder <tonyeder11@gmail.com>
2023-12-15 13:26:11 -0800

    reminder: dont use useState as a variable!

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,28 @@
 // create your App component here
+import React, { useState, useEffect } from 'react';
+
+function App() {
+    const [randomImage, setRandomImage] = useState("");
+    const [stateAs, setAsState] = useState(false);
+
+    useEffect(() => {
+        fetch('https://dog.ceo/api/breeds/image/random')
+            .then((res) => res.json())
+            .then((data) => {
+                console.log(data.message)
+                setRandomImage(data.message)
+                setAsState(true)
+
+            })
+    }, [])
+
+    if (!stateAs) return <p>Loading...</p>
+
+    return (
+        <div>
+            <img src={randomImage} alt="A Random Dog"></img>
+        </div>
+    )
+}
+
+export default App
